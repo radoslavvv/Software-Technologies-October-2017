fe0cdda224a7b46327753a96b857b2a32b653e1a
Radoslav <radoslav.varadinov@gmail.com>
2017-12-09 22:42:49 +0200

    added new solutions

---
+++ 05. CSharp/01. CSharp-Calculator/Calculator-CSharp/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System.Web.Mvc;
+using Calculator_CSharp.Models;
+
+namespace Calculator_CSharp.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index(Calculator calculator)
+        {
+            return View(calculator);
+        }
+
+        [HttpPost]
+        public ActionResult Calculate(Calculator calculator)
+        {
+            calculator.Result = CalculateResult(calculator);
+
+            return RedirectToAction("Index", calculator);
+        }
+
+        private decimal CalculateResult(Calculator calculator)
+        {
+            decimal result = 0;
+
+            switch (calculator.Operator)
+            {
+                case "+":
+                    result = calculator.LeftOperand + calculator.RightOperand;
+                    break;
+                case "-":
+                    result = calculator.LeftOperand - calculator.RightOperand;
+                    break;
+                case "*":
+                    result = calculator.LeftOperand * calculator.RightOperand;
+                    break;
+                case "/":
+                    result = calculator.LeftOperand / calculator.RightOperand;
+                    break;
+            }
+            return result;
+        }
+    }
+}
